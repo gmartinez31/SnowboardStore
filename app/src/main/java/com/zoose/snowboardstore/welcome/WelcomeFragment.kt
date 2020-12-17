ba34182945902a594ff7bc746faa8f52d4538ca7
gmartinez31 <bhgustavo13@yahoo.com>
2020-12-16 23:24:50 -0700

    welcome screen done

--- app/src/main/java/com/zoose/snowboardstore/welcome/WelcomeFragment.kt
+++ app/src/main/java/com/zoose/snowboardstore/welcome/WelcomeFragment.kt
@@ -7,25 +7,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 import com.zoose.snowboardstore.R
 import com.zoose.snowboardstore.databinding.WelcomeFragmentBinding
 
 class WelcomeFragment : Fragment() {
-
-//    private lateinit var viewModel: WelcomeViewModel
-
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val binding: WelcomeFragmentBinding = DataBindingUtil.inflate(
             inflater, R.layout.welcome_fragment, container, false)
 
+        binding.instructionBtn.setOnClickListener {
+            findNavController().navigate(WelcomeFragmentDirections.actionWelcomeDestinationToInstructionDestination())
+        }
         return binding.root
     }
-
-//    override fun onActivityCreated(savedInstanceState: Bundle?) {
-//        super.onActivityCreated(savedInstanceState)
-//        viewModel = ViewModelProvider(this).get(WelcomeViewModel::class.java)
-//        // TODO: Use the ViewModel
-//    }
-
 }
--- app/src/main/java/com/zoose/snowboardstore/welcome/WelcomeViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.zoose.snowboardstore.welcome
-
-import androidx.lifecycle.ViewModel
-
-class WelcomeViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
